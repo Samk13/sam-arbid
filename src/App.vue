d08ce810bf950fcc9d734f58aca671435138df92
Samk13 <samarbid13@gmail.com>
2020-11-30 02:10:45 +0100

    init app

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<section>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</section>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="flex flex-col">
+        <h1 class="bg-green-400 text-4xl p-4 text-center">{{ msg }}</h1>
+        <button @click="count++">count is: {{ count }}</button>
+        <p>
+            Edit <code>components/HelloWorld.vue</code> to test hot module
+            replacement.
+        </p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            count: 0,
+        }
+    },
+}
+</script>
