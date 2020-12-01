c2795bfb0403ec8eeeabbed149fe2f616f5f6309
Samk13 <samarbid13@gmail.com>
2020-12-01 03:28:26 +0100

    feed data from json to app comp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
     <section>
         <!-- <HelloWorld msg="Sam Arbid" /> -->
-        <cv-header />
+        <cv-header :data="data" />
+        <!-- {{ data }} -->
     </section>
 </template>
 
 <script>
 import { HelloWorld, CvHeader } from './components'
-
+import cvData from './data/cvData.json'
+import { reactive } from 'vue'
 export default {
     name: 'App',
     components: {
         HelloWorld,
         CvHeader,
+    },
+    setup() {
+        const data = reactive(cvData)
+
+        return {
+            data,
+        }
     },
 }
 </script>
--- src/components/CvHeader.vue
+++ src/components/CvHeader.vue
@@ -1,59 +1,38 @@
 <template>
     <section class="container m-auto flex flex-col">
         <div class="ml-4">
-            <h1 class="text-5xl font-black text-green-500">Sam Arbid</h1>
+            <h1 class="text-5xl font-black text-green-500">
+                {{ data.name }}
+            </h1>
             <hr class="border-2" />
-            <h2>Software Engineer</h2>
+            <h2>{{ data.title }}</h2>
         </div>
         <div>
             <ul>
-                <li>hammarby sjöstad/Sthlm/SE</li>
-                <li>samarbid13@gmail.com</li>
-                <li>github.com/Samk13</li>
-                <li>+46720342482</li>
+                <li>{{ data.address }}</li>
+                <li>{{ data.email }}</li>
+                <li>{{ data.github }}</li>
+                <li>{{ data.mobile }}</li>
             </ul>
         </div>
     </section>
     <section class="flex w-full justify-between">
-        <div class="text-4xl font-roboto font-black" id="left">
+        <div class="font-roboto font-black" id="left">
             <h1>Relevant Experience</h1>
-            <div id="capcito">
-                <h2>Software Engineer @ Capcito</h2>
-                <h2>december 2019 - present // Stockholm, SE</h2>
-                <ul>
-                    <li>
-                        Write clean, performant and robust code for a diverse
-                        array of client and internal projects
-                    </li>
-                    <li>
-                        Work with a variety of different languages, frameworks,
-                        and content management systems such as JavaScript,
-                        TypeScript, PHP, Laravel, Vue3, tailwindcss,
-                        composition-api, git , Jira, etc.
-                    </li>
-                    <li>
-                        Experience developing a complete digital interactive
-                        experience is an asset that sets me apart from both my
-                        peers and peers in my industry.
-                    </li>
-                </ul>
-            </div>
-            <div id="Firefighter">
-                <h2>Firefighter @ räddningstjänsten skaraborg</h2>
-                <h2>jan 2018 - november 2019//Karlsborg, SE</h2>
-                <ul>
-                    <li>
-                        After my career as a VFX artist and coming close to the
-                        oskar prize I looked for exploring a new career that is
-                        more useful for society. In the end i decided coding is
-                        the best way of doing this, but before that while I was
-                        living in the countryside, I served as a part time
-                        firefighter in Karlsborg. At the same time I got
-                        certified at lernia as carpenter and worked with
-                        swedishmodules in vara until I moved to the capital
-                        stockholm.
-                    </li>
-                </ul>
+            <div v-for="xp in data.relevantExperience" :key="xp.id">
+                <div id="capcito">
+                    <h2>
+                        <span>{{ xp.title }} @ {{ xp.place }}</span>
+                    </h2>
+                    <h2>
+                        <span>{{ xp.startDate }} - {{ xp.endDate }}</span>
+                    </h2>
+                    <ul v-for="(desc, index) in xp.description" :key="index">
+                        <li>
+                            {{ desc }}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
         <div class="text-4xl font-roboto font-black" id="right">Skills</div>
@@ -63,5 +42,11 @@
 <script>
 export default {
     name: 'CvHeader',
+    props: {
+        data: {
+            type: Object,
+            required: true,
+        },
+    },
 }
 </script>
