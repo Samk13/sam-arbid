d2014355053200cdd80fbbce8557adc88a314b7a
Samk13 <samarbid13@gmail.com>
2020-12-07 14:59:32 +0100

    skills component add some spacing

--- src/components/CvBodyRight.vue
+++ src/components/CvBodyRight.vue
@@ -1,57 +1,77 @@
 <template>
     <!-- Skills -->
-    <div v-if="data.skills">
+    <div v-if="data.skills" class="ml-3">
         <div class="text-4xl font-roboto font-black" id="right">Skills</div>
         <div>
             <h1 class="text-xl font-bold">Programing lamguages</h1>
-            <ul
+            <div
                 v-for="(lang, index) in data.skills.programingLanguages"
                 :key="index"
+                class="inline-block"
             >
-                <li>{{ lang }}</li>
-            </ul>
+                <span class="mr-2">{{ lang }}</span>
+            </div>
         </div>
         <div>
             <h1 class="text-xl font-bold">Libraries and Frameworks</h1>
-            <ul
+            <div
                 v-for="(framework, index) in data.skills.LibrariesFrameworks"
                 :key="index"
+                class="inline-block"
             >
-                <li>{{ framework }}</li>
-            </ul>
+                <div class="mr-2">{{ framework }}</div>
+            </div>
         </div>
         <div>
             <h1 class="text-xl font-bold">Tools & Platforms</h1>
-            <ul v-for="(plat, index) in data.skills.platforms" :key="index">
-                <li>{{ plat }}</li>
-            </ul>
+            <div
+                v-for="(plat, index) in data.skills.platforms"
+                :key="index"
+                class="inline-block"
+            >
+                <div class="mr-2">{{ plat }}</div>
+            </div>
         </div>
         <div>
             <h1 class="text-xl font-bold">Design</h1>
-            <ul v-for="(design, index) in data.skills.Design" :key="index">
-                <li>{{ design }}</li>
-            </ul>
+            <div
+                v-for="(design, index) in data.skills.Design"
+                :key="index"
+                class="inline-block"
+            >
+                <div class="mr-2">{{ design }}</div>
+            </div>
         </div>
         <!-- Education -->
         <div>
             <h1 class="text-xl font-bold">Education</h1>
-            <ul v-for="(educ, index) in data.Education" :key="index">
-                <li>
-                    <span>{{ educ.place }}</span>
-                    <span>{{ educ.startDate }}</span>
-                    <span>{{ educ.endDate }}</span>
+            <div
+                v-for="(educ, index) in data.Education"
+                :key="index"
+                class="inline-block"
+            >
+                <div class="mr-2">
+                    <span class="font-bold mr-2">{{ educ.place }}</span>
+                    <div class="inline-block">
+                        <span class="mr-2">{{ educ.startDate }}</span>
+                        <span>{{ educ.endDate }}</span>
+                    </div>
                     <span>{{ educ.location }}</span>
                     <p>{{ educ.description }}</p>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
         <div>
             <h1 class="text-xl font-bold">Intrest</h1>
-            <ul v-for="(intr, index) in data.Interests" :key="index">
-                <li>
+            <div
+                v-for="(intr, index) in data.Interests"
+                :key="index"
+                class="inline-block"
+            >
+                <div class="mr-2">
                     <span>{{ intr }}</span>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
